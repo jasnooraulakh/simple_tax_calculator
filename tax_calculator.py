53d75508cc77aa18f4c3a7f26d855ae9e87be351
Jasnoor Aulakh <jasgaming02singh@gmail.com>
2024-02-10 22:36:49 -0600

    remove redundant code

--- tax_calculator.py
+++ tax_calculator.py
@@ -3,8 +3,8 @@ class TaxCalculator:
     def __init__(self, income):
         self.income = income
         self.tax_amount = 0.00
-        self.tax_bracket = [
 
+        self.tax_bracket = [
             (0, 11000, 0.10),
             (11001, 44725, 0.12),
             (44726, 95375, 0.22),
@@ -15,9 +15,8 @@ class TaxCalculator:
         ]
 
     def calculate_tax(self):
-        """Calculate the tax owed amount across brackets."""
+        """Calculate the tax owed amount across brackets"""
         tax_owed = 0.00
-        previous_upper = 0
 
         for lower_limit, upper_limit, rate in self.tax_bracket:
             if self.income > lower_limit:
@@ -25,7 +24,6 @@ class TaxCalculator:
                 income_in_this_bracket = min(self.income, upper_limit) - lower_limit
                 # Accumulate the tax owed for the current bracket
                 tax_owed += income_in_this_bracket * rate
-                previous_upper = upper_limit
             else:
                 break
 
