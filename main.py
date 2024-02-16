3c7a75cc4674f71cf61ee016473bf8d4695ff650
Jasnoor Aulakh <jasgaming02singh@gmail.com>
2024-02-16 14:33:10 -0600

    reform code into project menu

--- main.py
+++
@@ -1,8 +0,0 @@
-from tax_calculator import TaxCalculator
-
-tax_calc = TaxCalculator()
-tax_calc.get_user_input()
-tax_calc.calculate_tax()
-tax_calc.print_details()
-# tax_calc.display_pie_chart()
-tax_calc.donut_chart()
--- project_menu.py
+++ project_menu.py
@@ -20,11 +20,14 @@ def get_user_input():
         filing_status_input = input("Enter your input (1 or 2): ")
 
         if filing_status_input.isdigit() and int(filing_status_input) in [1, 2]:
+            filing_status_input = int(filing_status_input)
 
             if filing_status_input == 1:
-                tax_calc.taxpayer_type == "single"
+                tax_calc.taxpayer_type = "single"
+                tax_calc.set_tax_bracket()
             elif filing_status_input == 2:
-                tax_calc.taxpayer_type == "married"
+                tax_calc.taxpayer_type = "married"
+                tax_calc.set_tax_bracket()
             break
         else:
             print("Invalid input")
@@ -48,9 +51,43 @@ def get_user_input():
     return tax_calc
 
 
+def menu():
+    print()
+    print("What would you like to do today?")
+    print("1. Tax Details")
+    print("2. Pie Chart")
+    print("3. Donut Chart")
+    print("4. Exit")
+
+    while True:
+        menu_input = input("Input: ")
+
+        if menu_input.isdigit() and int(menu_input) in [1, 2, 3, 4]:
+            return int(menu_input)
+        else:
+            print("Invalid input")
+
+
 def main():
     intro()
-    get_user_input()
+    tax_calc = get_user_input()
+    tax_calc.calculate_tax()
+
+    while True:
+        choice = menu()
+
+        if choice == 1:
+            tax_calc.print_details()
+        elif choice == 2:
+            tax_calc.display_pie_chart()
+        elif choice == 3:
+            tax_calc.donut_chart()
+        elif choice == 4:
+            print()
+            print("Thank you for using the Tax Calculator")
+            break
+        else:
+            print("Invalid input")
 
 
 if __name__ == "__main__":
